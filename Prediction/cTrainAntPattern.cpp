1e8fa0cdc2ee9ea95a992c4da67b67cf5a95bb54
magdaleen <magdaleen.ballot@up.ac.za>
2015-02-01 20:08:33 +0000

    update

--- Prediction/cTrainAntPattern.cpp
+++ Prediction/cTrainAntPattern.cpp
@@ -364,171 +364,176 @@ bool cTrainAntPattern::TrainANDSaveANDTest()
 	queryM += "filename) Values (";
 
 	mNumCells = mCells.size();
+	cout << " mNumCells = " << mNumCells << endl;
 
 	for (i=0; i<mNumCells; i++)
 	{
+
 		mCells[i].sNumOutputs = 1;
 		mCells[i].sNumInputs = 5;
 		mCells[i].sNumTrain = mCells[i].sMeasTrain.size();
 		mCells[i].sNumTest = mCells[i].sMeasTest.size();
+		cout << " NumTest = " <<  mCells[i].sNumTest << endl;
+		cout << " NumTrain = " <<  mCells[i].sNumTrain << endl;
 
-		mCells[i].sInputTrain = new double*[mCells[i].sNumTrain];
-		mCells[i].sOutputTrain = new double*[mCells[i].sNumTrain];
-		for (j=0; j < mCells[i].sNumTrain; j++)
-		{
-			mCells[i].sInputTrain[j] = new double[mCells[i].sNumInputs];
-			mCells[i].sOutputTrain[j] = new double[mCells[i].sNumOutputs];
-		}
-
-		for (j=0; j<mCells[i].sNumTest; j++)
+		if (mCells[i].sNumTrain>199)
 		{
 
-			mDEM.GetForLink(mCells[i].sPosition,	mCells[i].sMeasTrain[j].sLocation, mPlotResolution, mDEMProfile);
-			if (mUseClutter)
+			mCells[i].sInputTrain = new double*[mCells[i].sNumTrain];
+			mCells[i].sOutputTrain = new double*[mCells[i].sNumTrain];
+			for (j=0; j < mCells[i].sNumTrain; j++)
 			{
-				mClutter.GetForLink(mCells[i].sPosition,	mCells[i].sMeasTrain[j].sLocation, mPlotResolution, mClutterProfile);
+				mCells[i].sInputTrain[j] = new double[mCells[i].sNumInputs];
+				mCells[i].sOutputTrain[j] = new double[mCells[i].sNumOutputs];
 			}
-			mPathLoss.setParameters(mkFactor,mCells[i].sMeasTrain[j].sFrequency,
-						mCells[i].sHeight, MOBILEHEIGHT, mUseClutter, mClutterClassGroup);
-			PathLoss = mPathLoss.TotPathLoss(mDEMProfile, Tilt, mClutterProfile, DiffLoss);
-			Azimuth = mCells[i].sPosition.Bearing( mCells[i].sMeasTrain[j].sLocation);
 
-			mCells[i].sInputTrain[j][0] = 1;
-			mCells[i].sInputTrain[j][1] = cos(Azimuth*PI/180);
-			mCells[i].sInputTrain[j][2] = sin(Azimuth*PI/180);
-			mCells[i].sInputTrain[j][3] = cos(Tilt*PI/180);
-			mCells[i].sInputTrain[j][4] = sin(Tilt*PI/180);
+			for (j=0; j<mCells[i].sNumTrain; j++)
+			{
 
-			Delta = PathLoss + mCells[i].sMeasTrain[j].sMeasValue;
+				mDEM.GetForLink(mCells[i].sPosition,	mCells[i].sMeasTrain[j].sLocation, mPlotResolution, mDEMProfile);
+				if (mUseClutter)
+				{
+					mClutter.GetForLink(mCells[i].sPosition,	mCells[i].sMeasTrain[j].sLocation, mPlotResolution, mClutterProfile);
+				}
+				mPathLoss.setParameters(mkFactor,mCells[i].sMeasTrain[j].sFrequency,
+							mCells[i].sHeight, MOBILEHEIGHT, mUseClutter, mClutterClassGroup);
+				PathLoss = mPathLoss.TotPathLoss(mDEMProfile, Tilt, mClutterProfile, DiffLoss);
+				Azimuth = mCells[i].sPosition.Bearing( mCells[i].sMeasTrain[j].sLocation);
+
+				mCells[i].sInputTrain[j][0] = 1;
+				mCells[i].sInputTrain[j][1] = cos(Azimuth*PI/180);
+				mCells[i].sInputTrain[j][2] = sin(Azimuth*PI/180);
+				mCells[i].sInputTrain[j][3] = cos(Tilt*PI/180);
+				mCells[i].sInputTrain[j][4] = sin(Tilt*PI/180);
+
+				Delta = PathLoss + mCells[i].sMeasTrain[j].sMeasValue;
 			
-			mCells[i].sOutputTrain[j][0]  = (Delta - mCells[i].sMean) / 60;
-		}
-
-		mCells[i].sInputTest = new double*[mCells[i].sNumTest];
-		mCells[i].sOutputTest = new double*[mCells[i].sNumTest];
-		for (j=0; j < mCells[i].sNumTest; j++)
-		{
-			mCells[i].sInputTest[j] = new double[mCells[i].sNumInputs];
-			mCells[i].sOutputTest[j] = new double[mCells[i].sNumOutputs];
-		}
-
-		for (j=0; j<mCells[i].sMeasTest.size(); j++)
-		{
-
-			mDEM.GetForLink(mCells[i].sPosition,	mCells[i].sMeasTest[j].sLocation, mPlotResolution, mDEMProfile);
-			if (mUseClutter)
-			{
-				mClutter.GetForLink(mCells[i].sPosition,	mCells[i].sMeasTest[j].sLocation, mPlotResolution, mClutterProfile);
+				mCells[i].sOutputTrain[j][0]  = (Delta - mCells[i].sMean) / 60;
 			}
-			mPathLoss.setParameters(mkFactor,mCells[i].sMeasTest[j].sFrequency,
-						mCells[i].sHeight, MOBILEHEIGHT, mUseClutter, mClutterClassGroup);
-			PathLoss = mPathLoss.TotPathLoss(mDEMProfile, Tilt, mClutterProfile, DiffLoss);
-			Azimuth = mCells[i].sPosition.Bearing(mCells[i].sMeasTest[j].sLocation);
 
-			mCells[i].sInputTest[j][0] = 1;
-			mCells[i].sInputTest[j][1] = cos(Azimuth*PI/180);
-			mCells[i].sInputTest[j][2] = sin(Azimuth*PI/180);
-			mCells[i].sInputTest[j][3] = cos(Tilt*PI/180);
-			mCells[i].sInputTest[j][4] = sin(Tilt*PI/180);
+			mCells[i].sInputTest = new double*[mCells[i].sNumTest];
+			mCells[i].sOutputTest = new double*[mCells[i].sNumTest];
+			for (j=0; j < mCells[i].sNumTest; j++)
+			{
+				mCells[i].sInputTest[j] = new double[mCells[i].sNumInputs];
+				mCells[i].sOutputTest[j] = new double[mCells[i].sNumOutputs];
+			}
 
-			Delta = PathLoss + mCells[i].sMeasTest[j].sMeasValue;
+			for (j=0; j<mCells[i].sNumTest; j++)
+			{
+	
+				mDEM.GetForLink(mCells[i].sPosition,	mCells[i].sMeasTest[j].sLocation, mPlotResolution, mDEMProfile);
+				if (mUseClutter)
+				{
+					mClutter.GetForLink(mCells[i].sPosition,	mCells[i].sMeasTest[j].sLocation, mPlotResolution, mClutterProfile);
+				}
+				mPathLoss.setParameters(mkFactor,mCells[i].sMeasTest[j].sFrequency,
+							mCells[i].sHeight, MOBILEHEIGHT, mUseClutter, mClutterClassGroup);
+				PathLoss = mPathLoss.TotPathLoss(mDEMProfile, Tilt, mClutterProfile, DiffLoss);
+				Azimuth = mCells[i].sPosition.Bearing(mCells[i].sMeasTest[j].sLocation);
+	
+				mCells[i].sInputTest[j][0] = 1;
+				mCells[i].sInputTest[j][1] = cos(Azimuth*PI/180);
+				mCells[i].sInputTest[j][2] = sin(Azimuth*PI/180);
+				mCells[i].sInputTest[j][3] = cos(Tilt*PI/180);
+				mCells[i].sInputTest[j][4] = sin(Tilt*PI/180);
+	
+				Delta = PathLoss + mCells[i].sMeasTest[j].sMeasValue;
+				
+				mCells[i].sOutputTest[j][0]  = (Delta - mCells[i].sMean) / 60;
+			}
 			
-			mCells[i].sOutputTest[j][0]  = (Delta - mCells[i].sMean) / 60;
-		}
-		
-		TrainData.set_train_data(mCells[i].sNumTrain, 
-					mCells[i].sNumInputs, mCells[i].sInputTrain,
-					mCells[i].sNumOutputs, mCells[i].sOutputTrain);
+			TrainData.set_train_data(mCells[i].sNumTrain, 
+						mCells[i].sNumInputs, mCells[i].sInputTrain,
+						mCells[i].sNumOutputs, mCells[i].sOutputTrain);
+	
+			TestData.set_train_data(mCells[i].sNumTest, 
+						mCells[i].sNumInputs, mCells[i].sInputTest,
+						mCells[i].sNumOutputs, mCells[i].sOutputTest);
 
-		TestData.set_train_data(mCells[i].sNumTest, 
-					mCells[i].sNumInputs, mCells[i].sInputTest,
-					mCells[i].sNumOutputs, mCells[i].sOutputTest);
+			unsigned HiddenN1 = 3;
+			unsigned HiddenN2 = 2;
 
-		unsigned HiddenN1 = 3;
-		unsigned HiddenN2 = 2;
+			ANN.create_standard(3, mCells[i].sNumInputs, 
+						HiddenN1, HiddenN2, mCells[i].sNumOutputs);
+ 			ANN.set_train_error_function(FANN::ERRORFUNC_LINEAR);
+			ANN.set_train_stop_function(FANN::STOPFUNC_MSE);
+			ANN.set_training_algorithm(FANN::TRAIN_QUICKPROP);
+			ANN.set_activation_function_hidden(FANN::SIGMOID_SYMMETRIC);
+			ANN.set_activation_function_output(FANN::SIGMOID_SYMMETRIC);
+			ANN.randomize_weights(-0.50,0.50);
 
-		ANN.create_standard(4, mCells[i].sNumInputs, 
-					HiddenN1 ,HiddenN2, mCells[i].sNumOutputs);
- 		ANN.set_train_error_function(FANN::ERRORFUNC_LINEAR);
-		ANN.set_train_stop_function(FANN::STOPFUNC_MSE);
-		ANN.set_training_algorithm(FANN::TRAIN_QUICKPROP);
-		ANN.set_activation_function_hidden(FANN::SIGMOID_SYMMETRIC);
-		ANN.set_activation_function_output(FANN::SIGMOID_SYMMETRIC);
-		ANN.randomize_weights(-0.50,0.50);
-
-		cout << "saving ANN: Cell = " << mCells[i].sCI << "	i=" << i << endl;
-		gcvt(mCells[i].sCI,8,cell);
-		filename = outdir;
-		filename += "/";
-		filename += cell;
-		filename += temp;		
-//		time (&now);
-//		filename += ctime(&now);
-		filename += ".ann";
-
-//		ANN.train_on_data(TrainDataAngle, MAXepoch,REPORTInt,ERROR);
-
-		minTestError = MAXDOUBLE;
-		minTrainError = MAXDOUBLE;
-		stop = false;	
-		k=0;
-		while ((k<MAXepoch)&&(!stop))
-		{
-			TrainError = ANN.train_epoch(TrainData);
-			TestError = ANN.test_data(TestData);
-			if (0==k%REPORTInt)
+			cout << "saving ANN: Cell = " << mCells[i].sCI << "	i=" << i << endl;
+			gcvt(mCells[i].sCI,8,cell);
+			filename = outdir;
+			filename += "/";
+			filename += cell;
+//			time (&now);
+//			filename += ctime(&now);
+			filename += ".ann";
+	
+//			ANN.train_on_data(TrainDataAngle, MAXepoch,REPORTInt,ERROR);
+	
+			minTestError = MAXDOUBLE;
+			minTrainError = MAXDOUBLE;
+			stop = false;	
+			k=0;
+			while ((k<MAXepoch)&&(!stop))
 			{
-//				TestError = ANN.test_data(TestData);
-//				TestError = ANN.get_MSE();
-				cout << "celld = " << mCells[i].sCI << "	k=" << k 
-					<< "	TrainErr = " << TrainError 
-					<< "	TestErr = " << TestError << endl;
-			}
-			if ((TrainError <= minTrainError)&&(TestError<=minTestError)&&(k>MAXepoch/4))
+				TrainError = ANN.train_epoch(TrainData);
+				TestError = ANN.test_data(TestData);
+				if (0==k%REPORTInt)
+				{
+//					TestError = ANN.test_data(TestData);
+//					TestError = ANN.get_MSE();
+					cout << "celld = " << mCells[i].sCI << "	k=" << k 
+						<< "	TrainErr = " << TrainError 
+						<< "	TestErr = " << TestError << endl;
+				}
+				if ((TrainError <= minTrainError)&&(TestError<=minTestError)&&(k>MAXepoch/4))
+				{
+					ANN.save(filename);
+//					TestError = ANN.test_data(TestData);
+//					TestError = ANN.get_MSE();
+/*					cout << "cellid = " << mCells[i].sCI << "	k=" << k 
+						<< "	TrainErr = " << TrainError 
+						<< "	TestErr = " << TestError << endl;
+*/					stop = (TestError < ERROR)&&(TrainError < ERROR);
+					minTrainError = TrainError;
+					minTestError = TestError;
+				}
+//				else if (TestError>minTestError*1.05) stop = true;
+				k++;
+				
+			} 	
+
+			query = queryM;
+			gcvt(newANNid,9,annid);
+			query +=annid;
+			query +=",now(),";
+			query += machineid;
+			query += ",";
+			query +=cell;
+			query += ",'";
+			query += filename;
+			query += "');";
+	
+
+			if (!gDb.PerformRawSql(query))
 			{
-				ANN.save(filename);
-//				TestError = ANN.test_data(TestData);
-//				TestError = ANN.get_MSE();
-/*				cout << "cellid = " << mCells[i].sCI << "	k=" << k 
-					<< "	TrainErr = " << TrainError 
-					<< "	TestErr = " << TestError << endl;
-*/				stop = (TestError < ERROR)&&(TrainError < ERROR);
-				minTrainError = TrainError;
-				minTestError = TestError;
+				string err = "Error inserting AntNeuralNet by running query: ";
+				err += query;
+				cout << err <<endl; 
+				QRAP_WARN(err.c_str());
+				return false;
 			}
-//			else if (TestError>minTestError*1.05) stop = true;
-			k++;
-			
-		} 	
+	
+			ANN.save(filename);
+			ANN.destroy();
 
-		ANN.destroy();
-
-		query = queryM;
-		gcvt(newANNid,9,annid);
-		query +=annid;
-		query +=",now(),";
-		query += machineid;
-		query += ",";
-		query +=cell;
-		query += ",'";
-		query += filename;
-		query += "');";
-
-
-		if (!gDb.PerformRawSql(query))
-		{
-			string err = "Error inserting AntNeuralNet by running query: ";
-			err += query;
-			cout << err <<endl; 
-			QRAP_WARN(err.c_str());
-			return false;
+			newANNid++;
+			cout << "saved ANN: cell = " << mCells[i].sCI << "	i=" << i << endl;
 		}
-
-		ANN.save(filename);
-		ANN.destroy();
-
-		newANNid++;
-		cout << "saved ANN: cell = " << mCells[i].sCI << "	i=" << i << endl;
 	}
 	delete [] temp;
 	delete [] cell;

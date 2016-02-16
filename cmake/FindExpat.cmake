44b82fbea4f257035e0f5b7687549ab9fb27002e
magdaleen <magdaleen@252b22b1-1f8b-4092-b35f-88ebae8e1f94>
2016-02-16 11:01:03 +0000

    Preparing for new version

---
+++ cmake/FindExpat.cmake
@@ -0,0 +1,46 @@
+
+# CMake module to search for Expat library
+# (library for parsing XML files)
+#
+# If it's found it sets EXPAT_FOUND to TRUE
+# and following variables are set:
+#    EXPAT_INCLUDE_DIR
+#    EXPAT_LIBRARY
+
+
+FIND_PATH(EXPAT_INCLUDE_DIR expat.h 
+  /usr/local/include 
+  /usr/include 
+  "$ENV{LIB_DIR}/include/expat"
+  c:/msys/local/include
+  )
+#libexpat needed for msvc version
+FIND_LIBRARY(EXPAT_LIBRARY NAMES expat libexpat PATHS 
+  /usr/local/lib 
+  /usr/lib 
+  "$ENV{LIB_DIR}/lib"
+  c:/msys/local/lib
+  )
+
+IF (EXPAT_INCLUDE_DIR AND EXPAT_LIBRARY)
+   SET(EXPAT_FOUND TRUE)
+ENDIF (EXPAT_INCLUDE_DIR AND EXPAT_LIBRARY)
+
+
+IF (EXPAT_FOUND)
+
+   IF (NOT EXPAT_FIND_QUIETLY)
+      MESSAGE(STATUS "Found Expat: ${EXPAT_LIBRARY}")
+   ENDIF (NOT EXPAT_FIND_QUIETLY)
+
+ELSE (EXPAT_FOUND)
+
+   IF (EXPAT_FIND_REQUIRED)
+     MESSAGE(FATAL_ERROR "Could not find Expat")
+   ELSE (EXPAT_FIND_REQUIRED)
+     IF (NOT EXPAT_FIND_QUIETLY)
+        MESSAGE(STATUS "Could not find Expat")
+     ENDIF (NOT EXPAT_FIND_QUIETLY)
+   ENDIF (EXPAT_FIND_REQUIRED)
+
+ENDIF (EXPAT_FOUND)

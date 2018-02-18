a7d856e9673a4a7cc50d5391ab9b253a7068860a
magdaleen <magdaleen.ballot@up.ac.za>
2018-02-18 18:49:35 +0000

    Fixing

--- GP/main.cpp
+++ GP/main.cpp
@@ -14,6 +14,31 @@ using namespace std;
 int main (int argc, char * const argv[]) 
 {
 	cout << "Before constructing GP in main" << endl;
+
+	string queryC, queryN;
+	
+	if (!gSettings.LoadFromFile("/usr/lib/qgis/plugins/settings.xml"))
+		return 0;
+
+	if(!gDb.Connect ("postgres", "postqrap"))
+	{
+		cout << "Database connect failed:" << endl;
+		return 0;
+	}
+
+	cout << "Connected " << endl;
+
+  	queryC = "update coefficients set coefficient=0.0;";
+	if (!gDb.PerformRawSql(queryC))
+	{
+		cout << "Error clearing coefficients" << endl;
+	}
+
+  	queryN = "update qrap_config set value='false' where name = 'UseAntANN';";
+	if (!gDb.PerformRawSql(queryN))
+	{
+		cout << "Error updating qrap_config" << endl;
+	}
 	cGPpropModel GP;
 	GP.mainTuning();
     	return 0;
